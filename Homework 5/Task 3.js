647c9856005bb8e9f1df30009b7124185764aaca
IhorQQ <igor.golubtsov98@gmail.com>
2024-09-05 23:52:14 +0200

    Homework 5

---
+++ Homework 5/Task 3.js
@@ -0,0 +1,13 @@
+const number = 9;
+
+//for loop subtask
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number*i}`);
+}
+
+//while loop subtask
+let counter = 1
+while  (counter <= 10){
+    console.log(`${number} x ${counter} = ${number*counter}`);
+    counter++
+}
