8b3faab17aba21ffed1fa91c80ade53eef434f44
IhorQQ <igor.golubtsov98@gmail.com>
2024-09-23 13:17:28 +0200

    Homework 9

---
+++ Homework 9/Task 1.js
@@ -0,0 +1,8 @@
+const student = {}
+
+student.firstName = 'Ihor'
+student.lastName = 'Holubtsov'
+student.age = 26
+student.courses = ["Математика", "Історія", "Програмування"]
+
+console.log(student)
---
+++ Homework 9/Task 2.js
@@ -0,0 +1,9 @@
+const book = {
+    title: 'Harry Potter',
+    author: 'J. K. Rowling',
+    year: 1997
+}
+
+const {title, author} = book
+
+console.log(title, author)
