692e5b933208266f866a81fac55823a1f5ecb933
IhorQQ <igor.golubtsov98@gmail.com>
2024-11-27 21:14:39 +0100

    HW-23-finis+lint-fix

--- Homework-22-23/pageObjects/BasePage.js
+++ Homework-22-23/pageObjects/BasePage.js
@@ -1,31 +1,31 @@
 import { faker } from '@faker-js/faker';
 
 export default class BasePage {
-    constructor(page, context) {
-        this.context = context;
-        this.page = page;
-    }
+  constructor(page, context) {
+    this.context = context;
+    this.page = page;
+  }
 
-    async gotoBasePage() {
-        await this.page.goto('/');
-    }
+  async gotoBasePage() {
+    await this.page.goto('/');
+  }
 
 
-    static generateUserData() {
-        return {
-            firstName: faker.person.firstName().replace(/[^a-zA-Z\s]/g, ''),
-            lastName: faker.person.lastName().replace(/[^a-zA-Z\s]/g, ''),
-            email: `aqa-${Math.floor(Math.random()*10000).toString()}-${faker.internet.email({
-                firstName: faker.person.firstName().replace(/[^a-zA-Z\s]/g, ''),
-                provider: 'testemail.com',
-                allowSpecialCharacters: true
-            })}`,
-            password: faker.internet.password({
-                length: 8,
-                pattern: /[A-Za-z0-9]/,
-                prefix: 'A1a!'
-            }),
-        }
+  static generateUserData() {
+    return {
+      firstName: faker.person.firstName().replace(/[^a-zA-Z\s]/g, ''),
+      lastName: faker.person.lastName().replace(/[^a-zA-Z\s]/g, ''),
+      email: `aqa-${Math.floor(Math.random()*10000).toString()}-${faker.internet.email({
+        firstName: faker.person.firstName().replace(/[^a-zA-Z\s]/g, ''),
+        provider: 'testemail.com',
+        allowSpecialCharacters: true
+      })}`,
+      password: faker.internet.password({
+        length: 8,
+        pattern: /[A-Za-z0-9]/,
+        prefix: 'A1a!'
+      }),
     }
+  }
 
 }
--- Homework-22-23/pageObjects/SignUpModal.js
+++ Homework-22-23/pageObjects/SignUpModal.js
@@ -2,45 +2,45 @@ import BasePage from "./BasePage";
 
 export default class SignUpModal extends BasePage {
 
-    constructor(page) {
-        super(page)
-    }
+  constructor(page) {
+    super(page)
+  }
 
-    locators = {
-        firstNameField: this.page.locator('input[id="signupName"]'),
-        lastNameField: this.page.locator('input[id="signupLastName"]'),
-        emailField: this.page.locator('input[id="signupEmail"]'),
-        passwordField: this.page.locator('input[id="signupPassword"]'),
-        repeatPasswordField: this.page.locator('input[id="signupRepeatPassword"]'),
-        registerButton: this.page.getByRole('button', {name: 'Register'}),
-        validationError: this.page.locator('div[class="invalid-feedback"]'),
-    }
+  locators = {
+    firstNameField: this.page.locator('input[id="signupName"]'),
+    lastNameField: this.page.locator('input[id="signupLastName"]'),
+    emailField: this.page.locator('input[id="signupEmail"]'),
+    passwordField: this.page.locator('input[id="signupPassword"]'),
+    repeatPasswordField: this.page.locator('input[id="signupRepeatPassword"]'),
+    registerButton: this.page.getByRole('button', { name: 'Register' }),
+    validationError: this.page.locator('div[class="invalid-feedback"]'),
+  }
 
-    async fillFirstName (firstName) {
-        await this.locators.firstNameField.fill(firstName)
-    }
+  async fillFirstName (firstName) {
+    await this.locators.firstNameField.fill(firstName)
+  }
 
-    async fillLastName (lastName) {
-        await this.locators.lastNameField.fill(lastName)
-    }
+  async fillLastName (lastName) {
+    await this.locators.lastNameField.fill(lastName)
+  }
 
-    async fillEmail (email) {
-        await this.locators.emailField.fill(email)
-    }
+  async fillEmail (email) {
+    await this.locators.emailField.fill(email)
+  }
 
-    async fillPassword (password) {
-        await this.locators.passwordField.fill(password)
-    }
+  async fillPassword (password) {
+    await this.locators.passwordField.fill(password)
+  }
 
-    async fillRepeatPassword (repeatPassword) {
-        await this.locators.repeatPasswordField.fill(repeatPassword)
-    }
+  async fillRepeatPassword (repeatPassword) {
+    await this.locators.repeatPasswordField.fill(repeatPassword)
+  }
 
-    async fillSignUpForm ({firstName, lastName, email, password}) {
-        await this.fillFirstName(firstName)
-        await this.fillLastName(lastName)
-        await this.fillEmail(email)
-        await this.fillPassword(password)
-        await this.fillRepeatPassword(password)
-    }
+  async fillSignUpForm ({ firstName, lastName, email, password }) {
+    await this.fillFirstName(firstName)
+    await this.fillLastName(lastName)
+    await this.fillEmail(email)
+    await this.fillPassword(password)
+    await this.fillRepeatPassword(password)
+  }
 }
